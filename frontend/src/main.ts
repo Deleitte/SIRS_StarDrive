20670ce0dc9cabcecb4d35f21a1e4f787f1010b7
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2022-12-06 17:34:55 +0000

    Vuetify frontend layout

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,14 +2,13 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 
 import "vuetify/styles";
+import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
 import App from "./App.vue";
 import router from "./router";
-
-import "./assets/main.css";
 
 const app = createApp(App);
 
